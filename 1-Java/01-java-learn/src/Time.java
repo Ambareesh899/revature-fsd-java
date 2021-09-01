250fa045b841372ec88201860cbf1bf1056649c9
Ambareesh899 <75971081+Ambareesh899@users.noreply.github.com>
2021-09-01 11:14:07 +0530

    day 1 modified

--- 1-Java/01-java-learn/src/Time.java
+++ 1-Java/01-java-learn/src/Time.java
@@ -3,14 +3,8 @@ public class Time {
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
 		System.out.println("Enter Time ");
-		String Time=scan.next();
-		for(int i=0;i<Time.length();i++) {
-			char a=Time.charAt(i);
-			if(a==':'){
-				System.out.println(Time.charAt[i+1]);
-				break;
-			}
-		}
+		String time=scan.next();
+		System.out.println(time.substring(3,5));
 
 	}
 
